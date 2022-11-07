efe36e4552f1f9b942554e568735dc94515db67c
sayyed-ahsan <sayyed.ahsan.dev@gmail.com>
2022-11-07 23:53:16 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASSWORD);
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.l0zxzgu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('genius car server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Server running on ${port}`);
+})
